d2713e8061478adf4707d6093940e95ed9087839
Dournbrood <tanner1220002@gmail.com>
2019-12-13 12:44:33 -0500

    EXPOOOORTS!

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,5 @@
+//ACTIONS INDEX
+
+//Export action aliases and methods from local actions.
+
+export { REQUEST_SMURFS, SMURFS_RECEIVED, REQUEST_FAILED, POST_SMURF, SMURF_POSTED, POST_FAILED, requestSmurfs, postSmurf } from "./smurfActions"
---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,6 @@
+//REDUCER INDEX
+
+//Import your reducer from exact dir, and export it as default for directory-level access
+
+import reducer from "./smurfReducer";
+export default reducer;
